804e8956b3ec40657507728daa019cf346c38d44
JohnnyHiriart <johnnyhc@hotmail.com>
2022-06-08 14:05:44 +0200

    bug fix

--- interfaces/IAddress.ts
+++ interfaces/IAddress.ts
@@ -3,7 +3,7 @@ import { RowDataPacket } from 'mysql2';
 export default interface IAddress extends RowDataPacket {
   address1: string;
   address2: string;
-  postalCode: string;
+  postalCode: number;
   city: string;
   idUser: number;
 }
