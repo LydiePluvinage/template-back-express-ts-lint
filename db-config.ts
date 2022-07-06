858f4d09cf9b588d22fe8209ecd03701809752b8
Lydie Pluvinage <lydiepluvinage@yahoo.fr>
2022-07-06 13:55:33 +0200

    Database URL

--- db-config.ts
+++ db-config.ts
@@ -1,16 +1,10 @@
 // importe le package dotenv pour accèder au .env
 import 'dotenv/config';
 // importe mysql pour se connecter à la base
-import mysql, { Connection } from 'mysql2';
+import mysql, { Pool } from 'mysql2';
 
-// créer l'objet connection
-const connection: Connection = mysql.createConnection({
-  host: process.env.DB_HOST, // address of the server
-  port: Number(process.env.DB_PORT), // port of the DB server (mysql), not to be confused with the nodeJS server PORT !
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+// créer l'objet pool
+const pool: Pool = mysql.createPool(process.env.CLEARDB_DATABASE_URL || '');
 
-// exporte l'objet connection pour l'utiliser ailleurs
-export default connection;
+// exporte l'objet pool pour l'utiliser ailleurs
+export default pool;
