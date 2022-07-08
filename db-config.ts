c3558b7216ef02c9d2bdc2095208941370d0d384
Lydie Pluvinage <lydiepluvinage@yahoo.fr>
2022-07-08 10:39:44 +0200

    db-config without ts error

--- db-config.ts
+++ db-config.ts
@@ -3,8 +3,26 @@ import 'dotenv/config';
 // importe mysql pour se connecter à la base
 import mysql, { Pool } from 'mysql2';
 
+// importe la variable d'environnement
+let databaseUrl: string = process.env.CLEARDB_DATABASE_URL || '';
+// retire le type de base de données
+databaseUrl = databaseUrl.substring(8);
+// who doesn't love some good old effective Regex ?
+const [user, password, host, database] = databaseUrl.split(
+  /[:@/?)<>{}\[\]\r\n/\\]+/
+);
+
 // créer l'objet pool
-const pool: Pool = mysql.createPool(process.env.CLEARDB_DATABASE_URL || '');
+const pool: Pool = mysql.createPool({
+  host: host,
+  user: user,
+  password: password,
+  database: database,
+  port: 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
 // exporte l'objet pool pour l'utiliser ailleurs
 export default pool;
