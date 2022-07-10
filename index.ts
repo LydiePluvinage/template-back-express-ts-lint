91454a42a66ee35324b5b9aff5fa80c69ef042e4
Lydie Pluvinage <lydiepluvinage@yahoo.fr>
2022-07-10 23:18:41 +0200

    Heroku

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 import cookieParser from 'cookie-parser';
 import { handleError } from './helpers/errors';
 import setupRoutes from './router';
@@ -10,27 +10,29 @@ const port = process.env.PORT || 3000;
 
 // à faire des requetes axios
 const corsOptions: cors.CorsOptions = {
-  credentials: true,
+  // for cookies
+  // credentials: true,
+  // must-have for frontend to communicate with API
   origin: ['https://test-deploy-fullstack.vercel.app', 'http://localhost:3000'],
-  exposedHeaders: ['agreementrequired', 'Content-Range'],
+  // exposedHeaders: ['agreementrequired', 'Content-Range'],
 };
 
 // middleware cors
 app.use(cors(corsOptions));
 
 //middleware perso pour ajouter les headers nécessaires à react-admin et vercel
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE, OPTIONS'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin,X-Requested-With,Content-Type,Accept,Set-Cookie,X-PINGOTHER'
-  );
-  next();
-});
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//   res.setHeader('Content-Type', 'application/json');
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'GET, POST, PUT, DELETE, OPTIONS'
+//   );
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'Origin,X-Requested-With,Content-Type,Accept,Set-Cookie,X-PINGOTHER'
+//   );
+//   next();
+// });
 
 //middleware pour lire le body
 app.use(express.json());
