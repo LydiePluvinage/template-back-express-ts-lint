e34cc981ba2dc1941fb70722f7cc5634631d604e
Lydie Pluvinage <74747382+LydiePluvinage@users.noreply.github.com>
2022-07-18 11:42:22 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -19,6 +19,13 @@ const corsOptions: cors.CorsOptions = {
 // middleware cors
 app.use(cors(corsOptions));
 
+//middleware perso pour ajouter les headers nécessaires à react-admin
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Expose-Headers', 'Content-Range');
+  next();
+});
+
 //middleware pour lire le body
 app.use(express.json());
 //middleware pour envoyer des cookies
